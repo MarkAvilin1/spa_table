4682dd2a78ded4e5eb48f52536cead1b7ff6d1d5
Mark <46706090+MarkAvilin1@users.noreply.github.com>
2022-04-24 20:30:17 -0300

    Adding new function

--- table_app/static/JS/script.js
+++ table_app/static/JS/script.js
@@ -25,3 +25,30 @@ function ajax(page_no) {
         }
     })
 }
+
+function ajax2() {
+    let sorting = document.getElementById("id_sorting");
+    let categories = document.getElementById("id_categories");
+    let condition = document.getElementById("id_condition");
+    let choose = document.getElementById("id_choose");
+    $.ajax({
+        url: '/httpResponse',
+        type: "get",
+        data: {
+            'sorting': sorting.value,
+            'categories': categories.value,
+            'condition': condition.value,
+            'choose': choose.value
+        },
+        dataType: 'json'
+    }).done(function (data) {
+        let dataTable = data.map(function (data) {
+            return data.fields;
+        })
+        $('#table tbody').empty();
+        for (let i = 0; i < dataTable.length; i++) {
+            const element = dataTable[i];
+            $('#testTmpl').tmpl(element).appendTo('#table tbody');
+        }
+    })
+}
